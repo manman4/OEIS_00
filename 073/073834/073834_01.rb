a180b08bef18a3f6be5565e7ed8ddb572fdfc8ee
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-23 12:08:32 +0900

    Denominators of b(n) where b(1) = 1, b(i) = b(i-1) + 1/b(i-1).

---
+++ 073/073834/073834_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  a = 1r
+  ary = [1]
+  (2..n).each{|i|
+    a = a + 1 / a
+    ary << a
+  }
+  ary
+end
+
+n = 20
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i - 1].denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
