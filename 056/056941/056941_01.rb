e093f49d9bb8e7692decaf08eb5e1babc7e58ecd
manman4 <manchanr4@gmail.com>
2021-04-02 17:24:07 +0900

    Number of antichains (or order ideals) in the poset 5*m*n or plane partitions with not more than m rows, n columns and entries <= 5.

---
+++ 056/056941/056941_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def S(n, k)
+  (0..4).inject(1){|s, i| s * ncr(n + i, k) / ncr(k + i, k).to_r}.to_i
+end
+
+def A(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << S(i, j)
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
