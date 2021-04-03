5233d90c6805ee843e43032165def6cad0422d12
manman4 <manchanr4@gmail.com>
2021-04-03 17:16:58 +0900

    Number of ways to tile hexagon of edges n, n, n+1, n, n, n+1 with diamonds of side 1.

---
+++ 071/071094/071094_01.rb
@@ -0,0 +1,17 @@
+def f(k, n)
+  return 1 if k == 0
+  (n + 1..n + k).inject(:*)
+end
+
+def T(n, k)
+  (0..k - 1).inject(1){|s, i| s * f(n, i + n) / f(n, i).to_r}.to_i
+end
+
+n = 100
+(0..n).each{|i|
+  j = T(i, i + 1)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
