675d06018e8d0fa1f1c249d960c5cfe8971a0e75
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-29 21:19:38 +0900

    EULER transform of A001511.

---
+++ 092/092119/092119_01.rb
@@ -0,0 +1,33 @@
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def A092119(n)
+  I(A((0..Math.log(n, 2)).map{|i| [2 ** i, 1]}, n), n)
+end
+
+m = 11000
+n = 10000
+ary = A092119(m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
