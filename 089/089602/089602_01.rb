1dc21b319b9dfece245f8a328d6cc15cb7fe734a
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-09 23:20:43 +0900

    Expansion of L(x)^(1/4), where L(x) = o.g.f. for A053175.

---
+++ 089/089602/089602_01.rb
@@ -0,0 +1,29 @@
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
+
+def A(n)
+  a, b = 1, 8
+  cnt = 1
+  ary = [1]
+  while cnt <= n
+    cnt += 1
+    a, b = b, (8 * b * (3 * cnt * cnt - 3 * cnt + 1) - 128 * a * (cnt - 1) ** 2) / (cnt * cnt)
+    ary << a
+  end
+  ary
+end
+
+n = 900
+ary = sqrt_a(sqrt_a(A(n)))
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
