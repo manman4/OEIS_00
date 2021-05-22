3ab2e55b5c4e8a80c528c33b48210549515ca7a8
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-22 17:23:10 +0900

    Reciprocal of g.f. for A007863.

---
+++ 011/011365/011365_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n).map{|i| (0..i).inject(0){|s, j| s + ncr(i + j, i) * ncr(i + j + 1, i - j)} / (i + 1)}
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+n = 500
+ary = I(A(n), n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
