d580385de23864a1f74a024dc27e9fd58cc940d5
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-23 20:20:39 +0900

    E.g.f. exp( sinh(x) / exp(x) ) = exp( (1-exp(-2*x))/2 ).

---
+++ 009/009235/009235_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + (-k) ** (j - 1) * ncr(i - 1, j - 1) * ary[-j]}}
+  ary
+end
+
+n = 600
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
