dd99952dbf5ce3aced28b2c50c7a3776b2934b0b
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-30 02:37:59 +0900

    Table by antidiagonals of number of ways of choosing k items from n*k.

---
+++ 060/060539/060539_01.rb
@@ -0,0 +1,31 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def C(k, n)
+  (1..n).map{|i| ncr(k * i, k)}
+end
+
+def A322967(n)
+  a = []
+  (1..n).each{|i| a << C(i, n - i + 1)}
+  ary = []
+  (0..n - 1).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 140
+ary = A322967(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
