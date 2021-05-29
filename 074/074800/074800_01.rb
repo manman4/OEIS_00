e1d2c1a71ceedc2b9c4e55671fa721c38b17bedc
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-30 01:00:38 +0900

    a(1)=1; for n>1, a(n) is the denominator of b(n) = (4*n+1)*(Product_{i=1..n} (2*i-1) / Product_{i=1..n} (2*i))^5.

---
+++ 074/074800/074800_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  s = 1
+  ary = [1]
+  n.times{|i|
+    s *= ((2 * i + 1r) / (2 * i + 2)) ** 5
+    ary << s * (4 * i + 5)
+  }
+  ary
+end
+
+n = 400
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i].denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
