448116c62c87508a0ad62bf48b1ca7975d2d1f54
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-31 00:11:07 +0900

    Number of partitions of n in which the greatest part is 8.

---
+++ 026/026814/026814_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = (1..k).to_a
+  ps = Array.new(n + 1, 0)
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ([0] * k + ps)[0..n]
+end
+
+n = 10000
+ary = A(8, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
