41b43c7a0a0ab846c3cde307354ed87ef86757f3
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-31 11:40:35 +0900

    Square of the Euclidean length of the vector of Littlewood-Richardson coefficients of Sum_{lambda |- n} s_lambda^2, where s_lambda are the symmetric Schur functions and the sum runs over all partitions lambda of n.

---
+++ 067/067855/067855_01.rb
@@ -0,0 +1,24 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i).to_i}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 20
+ary1 = (0..n).map{|i| 1 / 2r}
+ary2 = (0..n).map{|i| 4}
+ary = A(ary1, ary2, n)
+(0..n).each{|i|
+  j = ary[i].to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
