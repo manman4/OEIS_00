5fcf12423e0a4c95f8065179dc0eeeb642130a27
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-31 22:54:51 +0900

    Numbers n such that 12*n divides F(12*n), where F(m) is the m-th Fibonacci number.

---
+++ 072/072378/072378_01.rb
@@ -0,0 +1,38 @@
+require 'matrix'
+
+def power(a, n, mod)
+  return Matrix.I(a.row_size) if n == 0
+  m = power(a, n >> 1, mod)
+  m = (m * m).map{|i| i % mod}
+  return m if n & 1 == 0
+  (m * a).map{|i| i % mod}
+end
+
+def f(m, n)
+  ary0 = Array.new(m, 0)
+  ary0[0] = 1
+  v = Vector.elements(ary0)
+  ary1 = [Array.new(m, 1)]
+  (0..m - 2).each{|i|
+    ary2 = Array.new(m, 0)
+    ary2[i] = 1
+    ary1 << ary2
+  }
+  # ”z—ñ‚ðˆø”‚É“WŠJ
+  a = Matrix[*ary1]
+  mod = n
+  # power(a, n, mod) * v‚Ì¬•ª‚Ímod–¢–ž
+  (power(a, n, mod) * v)[m - 1]
+end
+
+m = 2
+cnt = 1
+(1..10 ** 9).each{|i|
+  if f(m, 12 * i) == 0
+    print cnt
+    print ' '
+    puts i
+    cnt += 1
+  end
+  break if cnt > 10000
+}
