7a43c38d74a12afa463fd4d628d4529225170abc
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-31 22:46:19 +0900

    a(1) = a(2) = a(3) = a(4) = 1; a(n) = (a(n-1)*a(n-3) + a(n-2)^4)/a(n-4).

---
+++ 072/072877/072877_01.rb
@@ -0,0 +1,25 @@
+def A(m, n)
+  a = Array.new(4, 1)
+  ary = [1]
+  while ary.size < n
+    i = a[1] * a[3] + a[2] ** m
+    break if i % a[0] > 0
+    a = *a[1..-1], i / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+def A072877(n)
+  A(4, n)
+end
+
+n = 20
+ary = A072877(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
