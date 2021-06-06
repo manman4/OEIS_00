77327dea9a9bcfb5370327d375822371a40d41d6
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-06 21:46:20 +0900

    Denominator of coefficient of Pi^n in Ramanujan-like series for Zeta[4n+3].

---
+++ 057/057867/057867_01.rb
@@ -0,0 +1,33 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0]
+  }
+  ary
+end
+
+def A057867(n)
+  ary = bernoulli(4 * n)
+  (1..n).inject([]){|s, i|
+    j = 4 * i - 1
+    s << ((0..2 * i).inject(0){|t, k| t + (-1) ** (k + 1) * ary[2 * k] * ary[j + 1 - 2 * k] / (f(2 * k) * f(j + 1 - 2 * k) + 0r)} * 2 ** (j - 1)).denominator
+  }
+end
+
+n = 8
+ary = A057867(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
