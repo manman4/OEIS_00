b21e6796ed43ccfe7c42f242f093080034e02a74
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-06 22:01:31 +0900

    One of the two successive approximations up to 5^n for 5-adic integer sqrt(-1). Here the 3 (mod 5) case (except for n=0).

---
+++ 048/048899/048899_01.rb
@@ -0,0 +1,21 @@
+def A(k, n)
+  ary = [0]
+  a, mod = k, 5
+  n.times{|i|
+    b = a % mod
+    ary << b
+    a = b ** 5
+    mod *= 5
+  }
+  ary
+end
+
+n = 1500
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
