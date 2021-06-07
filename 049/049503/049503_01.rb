f5023725b8ee88f4c1b7b7b2fbfc539f67c41cee
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-08 00:07:00 +0900

    a(n) = A005130(n)^2.

---
+++ 049/049503/049503_01.rb
@@ -0,0 +1,25 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  s = 1
+  (0..n - 1).each{|i|
+    s *= f(3 * i + 1)
+  }
+  (0..n - 1).each{|i|
+    s /= f(i + n)
+  }
+  s
+end
+
+n = 100
+ary = (0..n).map{|i| A(i) ** 2}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
