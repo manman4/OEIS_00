b3864455fa7024076f8521f11ea3183c6aa692a7
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-07 23:49:59 +0900

    Revert transform of (-1 + x + x^2)/((x - 1)*(x + 1)).

---
+++ 049/049124/049124_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 1 if n == 0
+  (1..n).inject(0){|s, i| s + ncr(n, i) * ncr(2 * n + i, i - 1)} / n
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
