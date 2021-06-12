3896a356f45af428b893f41d3e89a47234107e44
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 02:13:19 +0900

    Number of integer-sided triangles with sides a,b,c, a<b<c, a+b+c=n that are right triangles.

---
+++ 024/024155/024155_01.rb
@@ -0,0 +1,19 @@
+n = 1000
+ary = Array.new(n + 1, 0)
+(1..n).each{|i|
+  (i..n).each{|j|
+    (j..n).each{|k|
+      ijk = i + j + k
+      break if ijk > n
+      if i + j > k
+        ary[ijk] += 1 if i * i + j * j == k * k
+      end
+    }
+  }
+}
+
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
