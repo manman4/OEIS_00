81791f30d4ce028170c2849c47b21bd259bb3d74
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 01:25:10 +0900

    Numerators of column 3 of table described in A051714/A051715.

---
+++ 051/051720/051720_01.rb
@@ -0,0 +1,20 @@
+def A(k, n)
+  ary = []
+  a = []
+  (0..n + k).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[k].numerator if i >=  k
+  }
+  ary
+end
+
+n = 650
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
