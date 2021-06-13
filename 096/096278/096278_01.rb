fb6b9813f12d8316591dbdb716839eb01a107a81
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 12:08:17 +0900

    Sums of successive sums of successive sums of successive primes.

---
+++ 096/096278/096278_01.rb
@@ -0,0 +1,19 @@
+require 'prime'
+
+def A(k, n)
+  a = Prime.take(n + k).to_a
+  k.times{|i|
+    a = (0..n + k - i - 2).map{|i| a[i + 1] + a[i]}
+  }
+  a
+end
+
+n = 10100
+ary = A(3, n)
+(1..10000).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
