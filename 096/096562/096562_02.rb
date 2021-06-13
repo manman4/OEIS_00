f9a20f70c3ea7f713dfe1bf0a7984490c744f945
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 12:14:14 +0900

    Coefficients of replicable function number "25a" with a(0) = -1.

---
+++ 096/096562/096562_02.rb
@@ -0,0 +1,26 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && i % k != 0
+  }
+  s
+end
+
+def B(k, n)
+  (1..n).map{|i| A(k, i)}
+end
+
+def C(k, m, n)
+  ary = [1]
+  a = [0] + B(k, n)
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - m * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 103
+ary = C(25, 1, n + 1)
+-1.upto(n){|i|
+  print i
+  print ' '
+  puts ary[i + 1]
+}
