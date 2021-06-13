5f2c5bfa743441fa3b3337952ca3459ad023b527
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 12:21:00 +0900

    Sum {0<d|n, n/d odd} d^11.

---
+++ 096/096963/096963_01.rb
@@ -0,0 +1,21 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += i ** k if (n / i) % 2 == 1
+    end
+  }
+  s
+end
+
+def B(k, n)
+  (1..n).map{|i| A(k, i)}
+end
+
+n = 10000
+ary = B(11, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
