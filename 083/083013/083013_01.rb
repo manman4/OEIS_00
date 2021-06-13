18c8533d03651207b80a2f54b04ee6274226ec28
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 20:48:31 +0900

    a(n) = Sum_{k=0..n-1} 9^k*B(k)*binomial(n,k) where B(k) is the k-th Bernoulli number.

---
+++ 083/083013/083013_01.rb
@@ -0,0 +1,36 @@
+def c(n, r)
+  return 1 if r == 0
+  return c(n, n - r) if r > n - r
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0]
+  }
+  ary
+end
+
+def A(k, n)
+  ary = bernoulli(n - 1)
+  ary[1] = -1r / 2
+  s = []
+  (0..n).each{|i|
+    s << (0..i - 1).inject(0){|t, j| t + k ** j * ary[j] * c(i, j)}.numerator
+  }
+  s
+end
+
+n = 20
+ary = A(9, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
