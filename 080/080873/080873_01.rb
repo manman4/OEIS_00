0f7d8e56b2d4e906beb2f0eb7293358f127af7f7
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 20:14:26 +0900

    a(n)*a(n+3) - a(n+1)*a(n+2) = 5, given a(0)=a(1)=1, a(2)=2.

---
+++ 080/080873/080873_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  a, b, c = 1, 1, 2
+  ary = [1]
+  while ary.size <= n
+    a, b, c = b, c, (5 + b * c) / a
+    ary << a
+  end
+  ary
+end
+
+n = 2000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
