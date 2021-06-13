67eadca72d81d30376c6ced907e92ec9639c649e
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 13:36:27 +0900

    Expansion of (Sum x^(n^2), n = -inf .. inf )^(-16).

---
+++ 004/004417/004417_01.rb
@@ -0,0 +1,22 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += (n / i) ** m if n % i == 0 && i % k > 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(2, 1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + 2 * k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10000
+ary = A(16, n)
+(0..n).each{|i|
+  j = (-1) ** (i % 2) * ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
