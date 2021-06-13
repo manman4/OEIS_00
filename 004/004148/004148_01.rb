48fd38dcf520462fd4fc051cc08b449265258f39
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 13:56:36 +0900

    Generalized Catalan numbers: a(n+1) = a(n) + Sum_{k=1..n-1} a(k)*a(n-1-k).

--- 004/004148/004148_01.rb
+++ 004/004148/004148_01.rb
@@ -2,7 +2,7 @@ def A004148(n)
   ary = [1]
   i = 0
   while i < n
-    ary << (1..i - 1).inject(ary[-1]){|s, j| s += ary[j] * ary[i - 1 - j]}
+    ary << (1..i - 1).inject(ary[-1]){|s, j| s + ary[j] * ary[i - 1 - j]}
     i += 1
   end
   ary
