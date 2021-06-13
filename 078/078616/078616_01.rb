32953e87b0c4308d60e28aa3685feee47b121038
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-13 17:44:00 +0900

    a(n) = Sum_{k=0..n} A010815(k).

---
+++ 078/078616/078616_01.rb
@@ -0,0 +1,23 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 109
+ary = A([[1, 1]], n)
+s = 0
+(0..n).each{|i|
+  s += ary[i]
+  break if s.to_s.size > 1000
+  print i
+  print ' '
+  puts s
+}
