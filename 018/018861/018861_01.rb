d58db560c3a3b2273d34ae9aac4f050ce64cbec6
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-15 21:36:03 +0900

    Smallest power of 5 that begins with n.

---
+++ 018/018861/018861_01.rb
@@ -0,0 +1,29 @@
+# 10^d - 1‚Ü‚Å
+def A(n, d)
+  ary = Array.new(10 ** d, -1)
+  ary[1] = 1
+  cnt = 1
+  f = 1
+  while cnt < 10 ** d - 1
+    f *= n
+    str = f.to_s
+    (0..d - 1).each{|i|
+      j = str[0..i].to_i
+      if ary[j] == -1
+        ary[j] = f
+        cnt += 1
+      end
+    }
+  end
+  ary
+end
+
+d = 3
+ary = A(5, d)
+(1..10 ** d - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
