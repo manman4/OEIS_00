d3566300fe18f6c3b32bb3b8f8753844778a5937
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-15 22:02:55 +0900

    3-multiplicative persistence: number of iterations of "multiply 3rd powers of digits" needed to reach 0 or 1.

---
+++ 031/031349/031349_01.rb
@@ -0,0 +1,25 @@
+def A(k, n)
+  s = 1
+  n.to_s.split('').each{|i|
+    s *= i.to_i ** k
+  }
+  s
+end
+
+def B(k, n)
+  cnt = 0
+  while n > 1
+    cnt += 1
+    n = A(k, n)
+  end
+  cnt
+end
+
+n = 10000
+(1..n).each{|i|
+  j = B(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
