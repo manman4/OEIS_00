7af493ee5ccb0c977dc696b9712a976a082670b5
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-15 21:49:23 +0900

    Number of lines through exactly 4 points of an n X n grid of points.

---
+++ 018/018811/018811_01.rb
@@ -0,0 +1,24 @@
+def f(n, k)
+  s = 0
+  m = n / k
+  (-m).upto(m){|x|
+    (-m).upto(m){|y|
+      if x.gcd(y) == 1
+        s += (n - (k * x).abs) * (n - (k * y).abs)
+      end
+    }
+  }
+  s
+end
+
+def g(k, n)
+  return 0 if n == 0
+  (f(n, k + 1) - 2 * f(n, k) + f(n, k - 1)) / 2
+end
+
+n = 1000
+(0..n).each{|i|
+  print i
+  print ' '
+  puts g(4, i)
+}
