39a377e92147db71aac9a460d320f54c0892377c
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-19 21:52:11 +0900

    a(n) = 5^n mod n.

---
+++ 066/066603/066603_01.rb
@@ -0,0 +1,19 @@
+def pow(a, m, mod)
+  return 1 if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+def A(k, n)
+  (1..n).map{|i| pow(k, i, i)}
+end
+
+n = 10000
+ary = A(5, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
