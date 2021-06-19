218fdf1833016b47d486cc4c76bc36d3c4f60160
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-19 20:43:47 +0900

    Square array read by antidiagonals: T(n,k) = number of partitions of n into distinct parts, each no more than k.

---
+++ 070/070936/070936_01.rb
@@ -0,0 +1,47 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1] + Array.new(n, 0)
+  (1..k).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, 1
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def A070936(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 139
+ary = A070936(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
