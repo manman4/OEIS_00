d587889606d66a9aa54528bc34b208428bf21f84
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-19 21:05:35 +0900

    Number of integer triangles with perimeter n having integral inradius.

---
+++ 070/070201/070201_01.rb
@@ -0,0 +1,28 @@
+def A(n)
+  cnt = 0
+  (1..n / 3).each{|a|
+    (a..(n - a) / 2).each{|b|
+      c = n - a - b
+      if a + b > c
+        s = n / 2r
+        t = (s - a) * (s - b) * (s - c) / s
+        if t.denominator == 1
+          t = t.to_i
+          cnt += 1 if Math.sqrt(t).to_i ** 2 == t
+        end
+      end
+    }
+  }
+  cnt
+end
+def A070201(n)
+  (1..n).map{|i| A(i)}
+end
+p A070201(100) 
+
+# n = 50
+# (1..n).each{|i|
+#   print i
+#   print ' '
+#   puts A(i)
+# }
