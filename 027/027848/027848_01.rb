99f828ef90f41e9482a82550dc7e4025187bdbf3
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-30 21:02:51 +0900

    a(n) = Sum_{ d|n } sigma(n/d)*d^4.

---
+++ 027/027848/027848_01.rb
@@ -0,0 +1,19 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(k, n)
+  (1..n).map{|i| (1..i).inject(0){|s, j| s + j ** k * s(j, i)}}
+end
+
+n = 46
+ary = A(3, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
