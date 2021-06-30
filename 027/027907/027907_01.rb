3854f55a5caa14ebf3361c8b6e6d6741b3d76584
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-30 21:05:43 +0900

    Irregular triangle of trinomial coefficients T(n,k) (n >= 0, 0 <= k <= 2*n), read by rows: n-th row is obtained by expanding (1 + x + x^2)^n.

---
+++ 027/027907/027907_01.rb
@@ -0,0 +1,31 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m���ȉ������o��
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+n = 7
+a = [1, 1, 1]
+ary = []
+(0..n).each{|i| ary << power(a, i, i * 2)}
+ary.flatten!
+(0..ary.size - 1).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
