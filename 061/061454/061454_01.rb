7490a7bece89404f8bfdf107fc36832fff90c4a9
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-04 00:04:01 +0900

    a(n) = denominator of the continued fraction [1; 2^2, 3^3, ..., n^n].

---
+++ 061/061454/061454_01.rb
@@ -0,0 +1,25 @@
+def f(n)
+  n ** n
+end
+
+def A(n)
+  a, b = 0, 1
+  ary = [1]
+  cnt = 0
+  while cnt < n
+    a, b = b, b * f(cnt + 2) + 1 * a
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 10
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
