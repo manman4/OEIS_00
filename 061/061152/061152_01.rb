86d50f6cbeb181ef9e6ad39a9ead910fba706dae
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-03 23:54:24 +0900

    Expansion of Product_{n>=1} (1+x^n)^prime(n).

---
+++ 061/061152/061152_01.rb
@@ -0,0 +1,44 @@
+require 'prime'
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  return I(power(ary, -n, m), m) if n < 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(n)
+  p_ary = [0] + Prime.take(n).to_a
+  a = [1] + [0] * n
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[i] = 1, 1
+    a = mul(a, power(b_ary, p_ary[i], n), n)
+  }
+  a
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
