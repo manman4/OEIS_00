a4bcfc2a78da2ce27818ae70ec0d709016277702
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-03 21:48:32 +0900

    Bessel polynomial y_n(-1).

---
+++ 000/000806/000806_01.rb
@@ -0,0 +1,19 @@
+def A000806(n)
+  ary = [1, 0]
+  i = 1
+  while i < n
+    i += 1
+    ary << (-2 * i + 1) * ary[-1] + ary[-2]
+  end
+  ary[0..n]
+end
+
+n = 500
+ary = A000806(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
