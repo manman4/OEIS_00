6d65687520c7f96bda5662254bd7391182f4351b
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-04 01:52:33 +0900

    Digit '2' concatenated with a(n) is a prime.

---
+++ 032/032665/032665_01.rb
@@ -0,0 +1,23 @@
+require 'prime'
+def A(k, n)
+  ary = []
+  str = k.to_s
+  s = 1
+  while ary.size < n
+    if !ary.include?(s)
+      if (str + s.to_s).to_i.prime?
+        ary << s
+      end
+    end
+    s += 1
+  end
+  ary
+end
+
+n = 10000
+ary = A(2, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
