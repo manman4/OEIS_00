a7aaf82aa2260bf01603a5636e5fcace72400581
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-03 23:19:43 +0900

    Figurate numbers based on the 10-dimensional regular convex polytope called the 10-dimensional cross-polytope, or 10-dimensional hyperoctahedron, which is represented by the Schlaefli symbol {3, 3, 3, 3, 3, 3, 3, 3, 4}. It is the dual of the 10-dimensional hypercube.

---
+++ 099/099197/099197_01.rb
@@ -0,0 +1,21 @@
+def A(k, m, n)
+  a, b = 0, 1
+  ary = [0]
+  cnt = 0
+  while cnt < n
+    a, b = b, 2 * m * b / (cnt + 1) + k * k * a
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 10000
+ary = A(1, 10, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
