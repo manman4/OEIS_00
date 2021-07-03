a7e6e1629240f69fff674325b81c50e04d45a009
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-03 22:47:03 +0900

    The odd bisection of A000594.

---
+++ 099/099059/099059_01.rb
@@ -0,0 +1,20 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A000594(n)
+  ary = [1]
+  a = [0] + (1..n - 1).map{|i| s(i)}
+  (1..n - 1).each{|i| ary << (1..i).inject(0){|s, j| s - 24 * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 26
+ary = A000594(2 * n + 1)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[2 * i]
+}
