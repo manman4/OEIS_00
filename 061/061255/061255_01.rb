eaefeb63c41b2d2a061f4397bd1f68e074a867d0
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-03 23:59:02 +0900

    Euler transform of Euler totient function phi(n), cf. A000010.

---
+++ 061/061255/061255_01.rb
@@ -0,0 +1,30 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def f(n)
+  s = 0
+  (1..n).each{|i| s += 1 if n.gcd(i) == 1}
+  s
+end
+
+n = 38
+ary0 = Array.new(n + 1, 1)
+ary1 = (0..n).map{|i| f(i)}
+ary = A(ary1, ary0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
