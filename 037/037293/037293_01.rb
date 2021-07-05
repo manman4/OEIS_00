c0e433031f4aab899878114fc36bca002b9cd57d
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-06 00:09:43 +0900

    a(n) = Sum_{i=0..2^(n-1)} binomial(2^(n-1), i)^2.

---
+++ 037/037293/037293_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 1 if n == 0
+  ncr(2 ** n, 2 ** (n - 1))
+end
+
+n = 20
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
