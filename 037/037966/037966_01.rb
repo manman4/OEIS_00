8d8534740142ba41b8ec75da0cf5e077d6e3ccc3
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-06 00:15:58 +0900

    a(n) = n^2*binomial(2*n-2, n-1).

---
+++ 037/037966/037966_01.rb
@@ -0,0 +1,20 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 0 if n == 0
+  n * n * ncr(2 * n - 2, n - 1)
+end
+
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
