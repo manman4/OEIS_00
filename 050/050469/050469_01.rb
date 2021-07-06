9b6c0a9018a558956d5061ab5d18d4872cbfa120
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-06 23:19:21 +0900

    a(n) = Sum_{ d divides n, n/d=1 mod 4} d - Sum_{ d divides n, n/d=3 mod 4} d.

---
+++ 050/050469/050469_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    1.step(i, 2){|j|
+      s += (-1) ** ((j - 1) / 2) * (i / j) ** k if i % j == 0
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 10000
+ary = A(1, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
