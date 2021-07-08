eb81d476ba028b09b262b6d4c3112ab289b21053
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-09 00:41:31 +0900

    Product_{k>=1} (1 + x^k)^a(k) = 1 + 2x.

---
+++ 038/038067/038067_01.rb
@@ -0,0 +1,21 @@
+def A(k, n)
+  ary = [0, k]
+  (2..n).each{|i|
+    s = -((-k) ** i)
+    (1..i - 1).each{|j|
+      s += (-1) ** (i / j) * j * ary[j] if i % j == 0
+    }
+    ary << s / i
+  }
+  ary[1..-1]
+end
+
+n = 3000
+ary = A(2, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
