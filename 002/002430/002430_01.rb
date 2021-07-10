c5a8d2083fbcd7abdd0e5f46942a5b9c5225eda1
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 01:18:57 +0900

    Numerators in Taylor series for tan(x). Also from Taylor series for tanh(x).

---
+++ 002/002430/002430_01.rb
@@ -0,0 +1,24 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0]
+  }
+  ary
+end
+def A(n)
+  a = bernoulli(2 * n)
+  (1..n).map{|i| (-1) ** (i % 2 + 1) * 2 ** (2 * i) * (2 ** (2 * i) - 1) * a[2 * i] / (1..2 * i).inject(:*)}
+end
+
+n = 300
+ary = A(n).map{|i| i.numerator}
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
