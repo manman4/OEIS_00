55ca4a118c382b7bb845bf1dd8ae55c0078cfd93
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 00:37:35 +0900

    Number of partitions of n into prime power parts (1 excluded).

---
+++ 023/023894/023894_01.rb
@@ -0,0 +1,27 @@
+require 'prime'
+
+def A(n)
+  a = []
+  (2..10 ** 7).each{|i|
+    break if a.size == n
+    b = i.prime_division
+    a << i if b.size == 1
+  }
+  ps = [1] + [0] * n
+  a.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+n = 10000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts ary[i]
+}
