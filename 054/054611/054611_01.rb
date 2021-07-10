fbbb20e55b87ab1769ff53c452e85910c2c2f34b
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-10 21:37:22 +0900

    a(n) = Sum_{d|n} phi(d)*4^(n/d).

---
+++ 054/054611/054611_01.rb
@@ -0,0 +1,20 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += phi(i) * 4 ** (n / i) if n % i == 0
+  }
+  s
+end
+
+n = 4000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
