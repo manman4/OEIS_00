6185274d24475bac1e1814a02c58c918a0ba3d4c
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 00:04:44 +0900

    Number of partitions of n into parts of 10 kinds.

---
+++ 023/023009/023009_01.rb
@@ -0,0 +1,19 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 26
+ary = A(-10, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
