1f555c96e53af20d796889f2a358f8014a0ef9e5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 01:07:15 +0900

    a(n) = 3^n reduced modulo 2^n.

---
+++ 002/002380/002380_01.rb
@@ -0,0 +1,21 @@
+def pow(a, m, mod)
+  return 1 % mod if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+def A(k, n)
+  (0..n).map{|i| pow(k, i, (k - 1) ** i)}
+end
+
+n = 4000
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts ary[i]
+}
