dc99fb1cdec56c0ff66d334d604b9e241fd9cc4b
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 00:03:00 +0900

    過去のファイル整理用

--- make_folder.rb
+++ make_folder.rb
@@ -1,11 +1,17 @@
 require 'fileutils'
 
-###################################################
+##################################################################
 #
 # 1. b_fileの名前に基づきフォルダ作成、b_file格納
+#
+#    ex. b012345.txt があるとき、012345/b012345.txt1に移動
+#
 # 2. フォルダに実行ファイル（rb, py）格納
 #
-###################################################
+#    ex. 012345.rb または 012345.py があるとき
+#    012345/b012345_01.rbに移動　（ファイル名に_01を追加）
+#
+##################################################################
 
 
 dir_name = "023"
@@ -33,6 +39,7 @@ dir.each{|file_name|
 dir.each{|file_name|
   if file_name[-3..-1] == ".rb" || file_name[-3..-1] == ".py"
     executable_file_name = file_name[0..-4]
+    extension = file_name[-3..-1]
     # 6桁＋α
     if executable_file_name.size == 6
       path = dir_name + "/" + executable_file_name
@@ -42,9 +49,9 @@ dir.each{|file_name|
       FileUtils.mkdir_p(path) unless FileTest.exist?(path)
 
       # executable_fileの移動
-      # 同名のファイルがなければ、フォルダの中に移動
-      if !(File.exists?(path + "/" + file_name))
-        FileUtils.mv(dir_name + "/" + file_name, path + "/" + file_name)
+      # 同名のファイル(_01.*)がなければ、フォルダの中に移動
+      if !(File.exists?(path + "/" + executable_file_name + "_01" + extension))
+        FileUtils.mv(dir_name + "/" + file_name, path + "/" + executable_file_name + "_01" + extension)
       end
     end
   end
