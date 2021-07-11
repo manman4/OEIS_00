b6eeae29c200c1fa2b2deaa236bc4a4d0fbb7c97
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 16:37:00 +0900

    a(n) = Sum_{d|8} phi(d)*n^(8/d).

---
+++ 054/054607/054607_01.rb
@@ -0,0 +1,20 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(k, n)
+  s = 0
+  (1..k).each{|i|
+    s += phi(i) * n ** (k / i) if k % i == 0
+  }
+  s
+end
+
+n = 10000
+(0..n).each{|i|
+  j = A(8, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
