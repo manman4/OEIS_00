c815b7dca9ff1f34b47127c88e08839b7a342f17
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 21:00:19 +0900

    Numerator of fraction equal to the continued fraction [ 0, 1, 4, ..., n^2 ].

---
+++ 036/036245/036245_01.rb
@@ -0,0 +1,21 @@
+def A(n)
+  a, b = 0, 1
+  ary = [1]
+  cnt = 0
+  while cnt < n
+    a, b = b, b * (cnt + 2) ** 2 + 1 * a
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 500
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
