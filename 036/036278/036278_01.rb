28f1f2390d03184584a542ee716102be8413a46a
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 21:01:15 +0900

    Denominators in Taylor series for cot x.

---
+++ 036/036278/036278_01.rb
@@ -0,0 +1,27 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0]
+  }
+  ary
+end
+
+# n‚Í-1‚©‚ç
+def A(n)
+  return [-1] if n == 1
+  a = bernoulli(2 * n + 2)
+  [1] + (1..n + 1).map{|i| (-1) ** (i % 2) * 2 ** (2 * i) * a[2 * i] / (1..2 * i).inject(:*)}
+end
+
+n = 300
+ary = A(n).map{|i| i.denominator}
+-1.upto(n){|i|
+  j = ary[i + 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
