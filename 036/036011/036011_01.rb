25e3309026504d09a3eb5cd6c596de91c7882b48
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-11 20:56:02 +0900

    Number of partitions of n into parts not of the form 25k, 25k+12 or 25k-12. Also number of partitions with at most 11 parts of size 1 and differences between parts at distance 11 are greater than 1.

---
+++ 036/036011/036011_01.rb
@@ -0,0 +1,26 @@
+def s(k, n)
+  m = 2 * k + 3
+  k1 = k + 1
+  s = 0
+  (1..n).each{|i|
+    j = i % m
+    s += i if (n % i == 0) && (j != 0) && (j != k1) && (j != (m - k1))}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(k, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 45
+ary = A(11, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
