c396c7346c0f0de6daecedb74bb84c38e5d1dc16
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-12 23:22:31 +0900

    Coefficients of the '5th-order' mock theta function F_1(q).

---
+++ 053/053265/053265_01.rb
@@ -0,0 +1,29 @@
+def A(k, n)
+  ary = []
+  1.step(2 * k + 2, 2){|i| ary << i}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+def A053265(n)
+  ary = Array.new(n + 1, 0)
+  (0..n).each{|i|
+    a = [0] * (2 * i * (i + 1)) + A(i, n)
+    (0..n).each{|j| ary[j] += a[j]}
+  }
+  ary
+end
+
+n = 1000
+ary = A053265(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
