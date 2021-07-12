644a5f6e36d29f18498a7cc49e351ceabde72325
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-12 23:10:48 +0900

    Row 3 of A007754.

---
+++ 058/058794/058794_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  a = Array.new(n + 1 + k, 1)
+  return a if k == 0
+  b = (1..n + 1 + k - 1).to_a
+  return b if k == 1
+  (k - 1).times{|i|
+    c = (0..n + k - 2 - i).map{|j| (b[j] * b[j + 1] - 1) / a[j + 1]}
+    a, b = b, c
+  }
+  b
+end
+
+n = 10000
+ary = A(3, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
