eab93c3d3223b5e7ba8093b4c13733198ceaa8a0
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-12 21:36:38 +0900

    Expansion of 1/(1-64*x)^(1/8), related to octo-factorial numbers A045755.

---
+++ 034/034977/034977_01.rb
@@ -0,0 +1,20 @@
+def A(k, n)
+  ary = [1]
+  s = 1
+  (0..n - 1).each{|i|
+    s *= (k * i + 1) * k
+    s /= (i + 1)
+    ary << s
+  }
+  ary
+end
+
+n = 500
+ary = A(8, n)
+(0..n).each{|i|
+  j = ary[i].to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
