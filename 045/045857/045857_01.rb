c833402301afb3fcb5604d684341261e3230838f
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-13 21:50:21 +0900

    Numbers n such that n^2 has initial digit '3'.

---
+++ 045/045857/045857_01.rb
@@ -0,0 +1,18 @@
+def A(k, n)
+  str = k.to_s
+  ary = []
+  i = 0
+  while ary.size < n
+    i += 1
+    ary << i if (i * i).to_s.split('').first == str
+  end
+  ary
+end
+
+n = 10000
+ary = A(3, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
