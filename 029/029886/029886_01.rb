8d0b6f550ca997c9ee74a8c391095924332ba8da
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-13 22:28:48 +0900

    Convolution of Thue-Morse sequence A001285 with itself.

---
+++ 029/029886/029886_01.rb
@@ -0,0 +1,34 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    if i % 2 == 0
+      ary << ary[i / 2]
+    else
+      ary << 3 - ary[i / 2]
+    end
+  }
+  ary
+end
+
+n = 10010
+a = A(n)
+ary = mul(a, a, n)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
