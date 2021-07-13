d33ada29d20345ce8043560d9fb240a4f0d7bf70
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-13 22:07:13 +0900

    Expansion of square root of q times normalized Hauptmodul for Gamma(4) in powers of q^8.

---
+++ 029/029838/029838_02.rb
@@ -0,0 +1,19 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += (-1) ** (n / i + 1) * i if n % i == 0 && i % k == m}
+  s
+end
+def A029838(n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(2, 1, i) - s(2, 0, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + ary[-j] * s_ary[j]} / i}
+  ary
+end
+
+n = 78
+ary = A029838(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
