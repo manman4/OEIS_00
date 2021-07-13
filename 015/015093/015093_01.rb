194fc6f904c8b7b6ee9f2944e01b6f556d5bd718
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-13 23:01:11 +0900

    Carlitz-Riordan q-Catalan numbers (recurrence version) for q=9.

---
+++ 015/015093/015093_01.rb
@@ -0,0 +1,18 @@
+def A(q, n)
+  ary = [1]
+  (1..n).each{|i| ary << (0..i - 1).inject(0){|s, j| s + q ** j * ary[j] * ary[i - 1 - j]}}
+  ary
+end
+def A015093(n)
+  A(9, n)
+end
+
+n = 100
+ary = A015093(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
