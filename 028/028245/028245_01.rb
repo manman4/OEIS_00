c44675bc2c0fd2a58dbbdc7fb1f401f5c152fde5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-14 23:15:46 +0900

    a(n) = 5^(n-1) - 4*4^(n-1) + 6*3^(n-1) - 4*2^(n-1) + 1 (essentially Stirling numbers of second kind).

---
+++ 028/028245/028245_01.rb
@@ -0,0 +1,23 @@
+def A(k, n)
+  a = [1]
+  i = 1
+  i > k ? ary = [1] : ary = [0]
+  while ary.size < n + 1
+    a << 0
+    b = [1] + (1..i).map{|j| (j + 1) * a[j] - j * a[j - 1]}
+    a = b
+    i += 1
+    i > k ? ary << a[k] : ary << 0
+  end
+  ary[0..n]
+end
+
+n = 2100
+ary = A(4, n - 1)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
