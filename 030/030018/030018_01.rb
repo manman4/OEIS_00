1f74366b48395c19546fb72c3a15e0954c165c3a
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-14 22:24:26 +0900

    Coefficients in 1/(1+P(x)), where P(x) is the generating function of the primes.

---
+++ 030/030018/030018_01.rb
@@ -0,0 +1,18 @@
+require 'prime'
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 41
+ary0 = [1] + Prime.take(n).to_a
+ary = I(ary0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
