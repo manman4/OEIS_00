7817a2734424316ec813e497b23aa910904aa155
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-17 01:09:51 +0900

    Number of n-stacks with strictly receding walls, or the number of Type A partitions of n in the sense of Auluck (1951).

---
+++ 001/001522/001522_01.rb
@@ -0,0 +1,32 @@
+def A001522(n)
+  ary = [1]
+  a = []
+  m = Math.sqrt(2 * n).to_i
+  (m + 1).times{a << Array.new(n + 1){0}}
+  a[0][0] = 1
+  (1..n).each{|x|
+    b = Marshal.load(Marshal.dump(a))
+    (1..m).each{|i| (0..n - x).each{|j| b[i][j + x] += a[i - 1][j]}}
+    a = b
+    # 1, 1, c , 1‚Ì^‚Á•½‚ç‚È‚à‚Ì
+    s = 1
+    # ŽRŒ^
+    (1..x - 1).each{|i|
+      # k + 1 <= m
+      (1..m - 1).each{|k|
+        s += a[k][i] * a[k + 1][x - i]
+        break if a[k][x] == 0
+      }
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 10000
+ary = A001522(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
