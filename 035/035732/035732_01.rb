8c69224a2e4aaa2711e7c6e65747c55bfe9ff996
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-17 16:51:38 +0900

    Coordination sequence for 37-dimensional cubic lattice.

---
+++ 035/035732/035732_01.rb
@@ -0,0 +1,22 @@
+def A(k, m, n)
+  a, b = 1, 2 * m
+  ary = [1]
+  cnt = 0
+  while cnt < n
+    a, b = b, (2 * m * b + k * k * cnt * a) / (cnt + 2r)
+    a = a.to_i if a.denominator == 1
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 10000
+ary = A(1, 37, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
