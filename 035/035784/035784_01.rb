28af56f5a19ea1dab8307fd22eebd50f4e785e98
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-17 14:46:00 +0900

    Coordination sequence for C_47 lattice.

---
+++ 035/035784/035784_01.rb
@@ -0,0 +1,23 @@
+def A(k, m, n)
+  a, b = 1, 2 * m
+  ary = [1]
+  cnt = 0
+  while cnt < n
+    a, b = b, (2 * m * b + k * k * cnt * a) / (cnt + 2r)
+    a = a.to_i if a.denominator == 1
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 10000
+a = A(1, 47, 2 * n)
+ary = (0..n).map{|i| a[2 * i]}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
