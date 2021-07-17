fb3007c003ae76be7c8fe60e3f96d2449a9b333e
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-18 03:20:05 +0900

    Expansion of 1/((1-x^3)*(1-x^4)*(1-x^5)).

---
+++ 008/008680/008680_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = (k + 1..2 * k + 1).to_a
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+n = 10000
+ary = A(2, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
