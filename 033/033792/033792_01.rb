b50032fb6f8b8094cbaeddebe5b5cdf91d23c0b2
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-17 22:03:54 +0900

    Product t2(q^d); d | 33, where t2(q) = theta2(q)/(2*q^(1/4)).

---
+++ 033/033792/033792_01.rb
@@ -0,0 +1,39 @@
+require 'prime'
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def d(m, k)
+  return m unless @factor[k]
+  (0..@factor[k][1]).map{|e| d(m * @factor[k][0] ** e, k + 1)}.flatten
+end
+
+def B(k, n)
+  @factor = Prime.prime_division(k).reverse
+  a = []
+  d(1, 0).each{|i|
+    a << [i * 2, 2]
+    a << [i, -1]
+  }
+  A(a, n)
+end
+
+n = 85
+ary = B(33, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
