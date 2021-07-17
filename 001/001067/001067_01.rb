88028c30ae7039840755afc82940276a0ab007e3
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-18 00:00:00 +0900

    Numerator of Bernoulli(2*n)/(2*n).

---
+++ 001/001067/001067_01.rb
@@ -0,0 +1,25 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A001067(n)
+  ary = bernoulli(2 * n)
+  (1..n).map{|i| (ary[2 * i] / (2 * i)).numerator}
+end
+
+n = 350
+ary = A001067(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
