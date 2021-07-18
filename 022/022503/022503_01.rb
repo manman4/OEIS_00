37487303d00112394c21c78e0017b7ddd2e996bc
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-18 19:38:53 +0900

    Describe the previous term! (method B - initial term is 7).

---
+++ 022/022503/022503_01.rb
@@ -0,0 +1,13 @@
+def lookandsay(str)
+  str.gsub(/(.)\1*/) {$1 + $&.length.to_s}
+end
+
+n = 100
+num = "7"
+(1..n).each{|i|
+  break if num.to_s.size > 1000
+  print i
+  print ' '
+  puts num
+  num = lookandsay(num)
+}
