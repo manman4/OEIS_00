549edb932d92c6bc0bfea81455349e641ec6e2bc
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-24 22:15:04 +0900

    Dirichlet inverse of sigma function (A000203).

---
+++ 046/046692/046692_01.rb
@@ -0,0 +1,28 @@
+require 'prime'
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+def A(k, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = [0]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[j] * m_ary[i / j] * j ** k if i % j == 0
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 78
+ary = A(1, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
