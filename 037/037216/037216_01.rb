21ac57f93568c1d8c0456b77c9f07eafbc7681ef
manman4 <7023566+manman4@users.noreply.github.com>
2021-09-14 21:13:32 +0900

    Expansion of ( Sum_{k>=0} k*q^(k^2) )^4.

---
+++ 037/037216/037216_01.rb
@@ -0,0 +1,42 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, m, n)
+  ary = Array.new(n + 1, 0)
+  s = 0
+  t = s ** k
+  while t <= n
+    ary[t] = s
+    s += 1
+    t = s ** k
+  end
+  power(ary, m, n)
+end
+
+n = 10000
+ary = A(2, 4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
