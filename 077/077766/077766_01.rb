6bbb758e5b70c43cb31d9dcef393f95008ca458f
manman4 <7023566+manman4@users.noreply.github.com>
2021-10-07 10:06:54 +0900

    Number of primes of form 4k+1 between n^2 and (n+1)^2.

---
+++ 077/077766/077766_01.rb
@@ -0,0 +1,18 @@
+require 'prime'
+
+def A(k, n)
+  s = 0
+  (n ** 2..(n + 1) ** 2).each{|i|
+    if i.prime?
+      s += 1 if i % 4 == k
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i| 
+  print i
+  print ' '
+  puts A(1, i)
+}
