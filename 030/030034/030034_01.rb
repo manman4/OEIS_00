5a23a618cb8df2fb2edfc677da645284ca3b6f58
manman4 <7023566+manman4@users.noreply.github.com>
2021-10-15 20:23:04 +0900

    a(n+1) = Sum_{k=0..floor(n/4)} a(k) * a(n-k).

---
+++ 030/030034/030034_01.rb
@@ -0,0 +1,17 @@
+def A(k, m, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..k * (i - 1) / m).inject(0){|s, j| s + ary[j] * ary[i - 1 - j]}
+  }
+  ary
+end
+
+n = 1000
+ary = A(1, 4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
