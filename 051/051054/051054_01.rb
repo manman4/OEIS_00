191f32ff46f88cc3a4392f3bb09a6b1a806881bd
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-06 21:53:51 +0900

    a(n) = Sum_{k=1..n} C(n, floor(n/k)).

---
+++ 051/051054/051054_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 0 if n == 0
+  (1..n).inject(0){|s, i| s + ncr(n, n / i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
