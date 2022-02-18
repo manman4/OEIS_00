5b4d74704c4883e564162f8e436c00680298ad47
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-18 23:49:34 +0900

    If n = Product (p_j^k_j) then a(n) = Product (p_j) + Product (k_j).

---
+++ 039/039696/039696_01.rb
@@ -0,0 +1,14 @@
+require 'prime'
+
+def A(n)
+  return 2 if n == 1
+  a = n.prime_division.to_a
+  a.map{|i| i[0]}.inject(:*) + a.map{|i| i[1]}.inject(:*)
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i)
+}
