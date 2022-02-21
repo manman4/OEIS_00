c54939a10737005a05bb15a841f49c5fd9a0a75b
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-21 22:31:29 +0900

    Fifth-from-right diagonal of triangle A121207.

---
+++ 045/045500/045500_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..i - 1).inject(0){|s, j| s + ncr(i + k - 2, j + k - 1) * ary[j]}
+  }
+  ary
+end
+
+n = 600
+ary = A(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
