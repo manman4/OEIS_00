7f535c8f7959150e3da9fd545c41774cd15eb82c
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-02 21:58:13 +0900

    Number of 8's in n!.

---
+++ 079/079693/079693_01.rb
@@ -0,0 +1,21 @@
+def A(k, n)
+  m = k.to_s
+  s = 1
+  ary = [0]
+  (1..n).each{|i|
+    s *= i
+    ary << s.to_s.count(m)
+  }
+  ary
+end
+
+n = 10100
+m = 10000
+ary = A(8, n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
