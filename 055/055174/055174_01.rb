b72d722a8b4dfaff2708a90ad7572644b55543f6
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-12 04:01:17 +0900

    Cumulative counting sequence: method B (noun,adjective)-pairs with first term 2.

---
+++ 055/055174/055174_01.rb
@@ -0,0 +1,16 @@
+def A(k, n)
+  ary = [k]
+  (1..n).each{|i|
+    ary += ary.uniq.map{|i| [i, ary.count(i)]}
+    ary.flatten!
+  }
+  ary
+end
+
+n = 100
+ary = A(2, n)
+(1..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
