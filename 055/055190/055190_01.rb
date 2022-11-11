e1ad208117a559b716a687fc92ad04dadd9f6ebc
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-12 02:16:58 +0900

    Cumulative counting sequence: method A (adjective-before-noun) pairs, starting with 4.

---
+++ 055/055190/055190_01.rb
@@ -0,0 +1,16 @@
+def A(k, n)
+  ary = [k]
+  (1..n).each{|i|
+    ary += ary.uniq.map{|i| [ary.count(i), i]}
+    ary.flatten!
+  }
+  ary
+end
+
+n = 100
+ary = A(4, n)
+(1..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
