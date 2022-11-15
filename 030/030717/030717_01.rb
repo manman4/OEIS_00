025591776a9e13d00f1b50eb7588edb23812df38
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-16 01:25:13 +0900

    The first list after the following procedure: starting with a list [1] and an empty list, repeatedly add the distinct values already in the first list in ascending order to the second list and add the corresponding frequencies of those values to the first list.

--- 030/030717/030717_01.rb
+++ 030/030717/030717_01.rb
@@ -1,6 +1,6 @@
 def A(n)
   ary = [1]
-  (1..n).each{|i|
+  n.times{
     ary += ary.uniq.sort.map{|i| ary.count(i)}
   }
   ary
@@ -9,7 +9,7 @@ end
 n = 100
 # sortがないとa(114)で微妙に異なる値のものがでてくる
 ary = A(n)
-(1..1000).each{|i|
+(1..10000).each{|i|
   print i
   print ' '
   puts ary[i - 1]
