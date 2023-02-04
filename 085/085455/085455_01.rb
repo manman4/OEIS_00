08f0fb5ce3c29f1b00d7855a35d86265a01a7da1
manman4 <7023566+manman4@users.noreply.github.com>
2023-02-04 16:42:52 +0900

    Sum_{i=0..n} Sum_{j=0..i} a(j) * a(i-j) = (-3)^n.

---
+++ 085/085455/085455_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + k ** i * ncr(n - 1, n - i) * ncr(2 * i, i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(-1, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
