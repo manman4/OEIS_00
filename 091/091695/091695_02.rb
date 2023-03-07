f96517f6373725172864c5357444435eaa0a5cc4
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-07 23:38:53 +0900

    Expansion of e.g.f. exp(x/(1-x)^3).

---
+++ 091/091695/091695_02.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  m = f(n)
+  (0..n).inject(0){|s, i| s + ncr(n + (k - 1) * i - 1, n - i) * m / f(i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
