68c4c36f6ff8cbb65975006283073ca554d8d96c
manman4 <7023566+manman4@users.noreply.github.com>
2023-05-16 00:27:42 +0900

    Number of partitions of n into an even number of parts, the least being 2; also, a(n+2) = number of partitions of n into an odd number of parts, each >=2.

--- 027/027194/027194_01.rb
+++ 027/027194/027194_01.rb
@@ -4,7 +4,7 @@ def partition(n, min, max)
   [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
 end
 
-def A(k, n)
+def A(n, k)
   cnt = 0
   partition(n, k, n).each{|i|
     cnt += 1 if i.size % 2 == 1
@@ -15,7 +15,7 @@ end
 n = 30
 b = []
 (1..n).each{|i|
-  j = A(2, i - 2)
+  j = A(i - 2, 2)
   break if j.to_s.size > 1000
   print i
   print ' '
