fae5b84f22a15846192be3656ef3511e2966ee33
manman4 <7023566+manman4@users.noreply.github.com>
2023-05-16 00:31:29 +0900

    Number of partitions of n into an odd number of parts, the least being 6; also, a(n+6) = number of partitions of n into an even number of parts, each >=6.

---
+++ 027/027192/027192_01.rb
@@ -0,0 +1,25 @@
+# 和因子はmin以上max以下
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(n, k)
+  cnt = 0
+  partition(n, k, n).each{|i|
+    cnt += 1 if i.size % 2 == 0
+  }
+  cnt
+end
+
+n = 50
+b = []
+(1..n).each{|i|
+  j = A(i - 6, 6)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+  b << j
+}
+p b
