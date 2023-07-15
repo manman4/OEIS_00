695a4d8429c4a4bf6e5d2f4b1ae4e0025c17a379
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-15 21:43:20 +0900

    Digits of powers of 11.

---
+++ 008/008571/008571_01.rb
@@ -0,0 +1,17 @@
+def A(n, k)
+  s = 1
+  ary = [1]
+  (1..n).each{|i|
+    s *= k
+    ary << s.to_s.split('').map(&:to_i)
+  }
+  ary.flatten
+end
+
+n = 100
+ary = A(n, 11)
+(0..ary.size - 1).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
