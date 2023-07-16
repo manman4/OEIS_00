d3e0c59bbde9c594c6eb46790601322e0030fb15
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-16 22:54:53 +0900

    Positive integers k such that k! is divisible by (k+1)^7.

---
+++ 061/061755/061755_01.rb
@@ -0,0 +1,20 @@
+# Numbers k such that k! is divisible by (k+1)^m.
+def A(k, n)
+  ary = []
+  cnt = 1
+  s = 1
+  while ary.size < n
+    ary << cnt if s % (cnt + 1) ** k == 0
+    cnt += 1
+    s *= cnt
+  end
+  ary
+end
+
+n = 10000
+ary = A(7, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
