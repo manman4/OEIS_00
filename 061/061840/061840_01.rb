4e6ff2f376cab20a22dfc1e18aa731012fbdc90c
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-17 19:22:45 +0900

    Numbers k such that (k+4)^4 | k!.

---
+++ 061/061840/061840_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = []
+  cnt = 1
+  s = 1
+  while ary.size < n
+    ary << cnt if s % (cnt + k) ** k == 0
+    cnt += 1
+    s *= cnt
+  end
+  ary
+end
+
+n = 10000
+ary = A(4, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
