b1a6259f5d899be9e46eccd6f72b9235bfc83d72
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-24 01:04:30 +0900

    Number of sums i^3 + j^3 that occur more than once for 1<=i<=j<=n.

---
+++ 061/061798/061798_01.rb
@@ -0,0 +1,24 @@
+def A(n)
+  h = {}
+  (1..n).each{|i|
+    (i..n).each{|j|
+      k = i * i * i + j * j * j
+      if h.has_key?(k)
+        h[k] += 1
+      else
+        h[k] = 1
+      end
+    }
+  }
+  # 1以上
+  h.to_a.select{|i| i[1] > 1}.size
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
