aa665831b411b66189ba6e7b8ca03b154444628f
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-31 20:59:03 +0900

    Triangle with a(n,k) = C(n,floor(n/k)) with n>=k>=1.

---
+++ 066/066704/066704_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (1..n).map{|i| (1..i).map{|j| ncr(i, i / j)}}.flatten
+end
+
+n = 140
+ary = A(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
