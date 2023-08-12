c90a6d5cf5ad52a9cfa50ca4fc3218de4a1cc1bd
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-12 13:52:53 +0900

    Table T(n,k) by antidiagonals of floor[n^k/k] [n,k >= 1].

---
+++ 050/050155/060155_01.rb
@@ -0,0 +1,23 @@
+def A(k, n)
+  n ** k / k
+end
+
+def B(n)
+  ary = []
+  (1..n).each{|i|
+    (1..i).each{|j|
+      ary << A(i - j + 1, j)
+    }
+  }
+  ary
+end
+
+n = 140
+ary = B(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
