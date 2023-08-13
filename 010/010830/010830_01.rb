8e88dc8671861d11c179258335e9b9eeb3618afe
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-13 19:56:38 +0900

    Expansion of Product_{k>=1} (1-x^k)^25.

---
+++ 010/010830/010830_01.rb
@@ -0,0 +1,23 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A(m, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - m * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10100
+m = 10000
+ary = A(25, n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
