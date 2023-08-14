fc337abd726c3ff177c00183a721bcbbbb0f1927
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-14 15:37:27 +0900

    a(n) = n! * (n+1)! * (n+2)! * (n+3)! / (2! * 3! * 4!).

---
+++ 010/010798/010798_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n == 0
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(2) * f(3) * f(4)
+  return f(n) * f(n + 1) * f(n + 2) * f(n + 3) / m
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
