1ff14d59bbbdee7c579ffdec84d61a52e1232c98
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-23 21:34:44 +0900

    Number of ternary search trees on n keys.

---
+++ 019/019497/019497_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  return 1 if n == 0
+  (0..n - 1).inject(0){|s, i| s + ncr(n - i, i) * ncr(k * i, n - i - 1) / (n - i).to_r}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i) 
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
