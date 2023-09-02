90729bb810144b28263746f0d0be24e62de9b733
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-02 20:36:55 +0900

    Binomial coefficient C(4n,n-9).

---
+++ 004/004339/004339_01.rb
@@ -0,0 +1,13 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+n = 1000
+(9..n).each{|i|
+  j = ncr(4 * i, i - 9)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
