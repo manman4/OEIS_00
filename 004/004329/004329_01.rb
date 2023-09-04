e598169be52802c6741dcbcbc18376c5e8a95359
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-04 17:29:24 +0900

    Binomial coefficient C(3n,n-11).

---
+++ 004/004329/004329_01.rb
@@ -0,0 +1,13 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+n = 1000
+(11..n).each{|i|
+  j = ncr(3 * i, i - 11)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
