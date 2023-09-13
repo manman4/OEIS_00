3dfa3211777e80e078c181b57f9df4cb2d25088f
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-13 18:58:09 +0900

    Numbers with exactly 6 2's in ternary expansion.

---
+++ 023/023704/023704_01.rb
@@ -0,0 +1,22 @@
+# kがm個
+def A(k, m, n)
+  n.to_s(3).split('').map(&:to_i).count(k) == m
+end
+
+def B(k, m, n)
+  ary = []
+  s = 1
+  while ary.size < n
+    ary << s if A(k, m, s)
+    s += 1
+  end
+  ary
+end
+
+n = 10000
+ary = B(2, 6, n)
+(1..ary.size).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
