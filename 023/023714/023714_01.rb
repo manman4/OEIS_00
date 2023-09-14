5a1974cdce73d596ec67cc7bf6834a05486c48a8
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-14 19:43:47 +0900

    Numbers with a single 2 in their base 4 expansion.

---
+++ 023/023714/023714_01.rb
@@ -0,0 +1,22 @@
+# kがm個
+def A(k, m, n)
+  n.to_s(4).split('').map(&:to_i).count(k) == m
+end
+
+def B(k, m, n)
+  ary = []
+  s = 0
+  while ary.size < n
+    ary << s if A(k, m, s)
+    s += 1
+  end
+  ary
+end
+
+n = 10000
+ary = B(2, 1, n)
+(1..ary.size).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
