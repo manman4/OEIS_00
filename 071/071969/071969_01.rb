c6c41af2040fc91d3cac60a034bae371242dea6e
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-27 22:02:13 +0900

    a(n) = Sum_{k=0..floor(n/3)} (binomial(n+1, k)*binomial(2*n-3*k, n-3*k)/(n+1)).

---
+++ 071/071969/071969_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n / k).inject(0){|s, i| s + ncr(n + 1, i) * ncr(2 * n - k * i, n - k * i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
