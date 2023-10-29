3942238a1a332927aac96cbac1f85d51631ca4d9
manman4 <7023566+manman4@users.noreply.github.com>
2023-10-29 20:33:03 +0900

    1 + Sum_{n=1..inf} a_n x^n = 1/Product_{n=1..inf} (1+x^n)^prime(n).

---
+++ 061/061151/061151_01.rb
@@ -0,0 +1,28 @@
+require 'prime'
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10100
+m = 10000
+ary1 = [0] + Prime.take(n).to_a
+ary2 = [-1] * (n + 1)
+ary = A(ary1, ary2, n)
+
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
