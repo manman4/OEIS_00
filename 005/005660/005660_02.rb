d541bbb8fe09f63500da997d9b83e521e387c7a4
manman4 <7023566+manman4@users.noreply.github.com>
2024-03-01 01:33:36 +0900

    If k appears so do 2k+2 and 3k+3. (duplicates omitted.)

---
+++ 005/005660/005660_02.rb
@@ -0,0 +1,18 @@
+# If n appears so do 2n+2 and 3n+3.
+def A(n)
+  a = [3]
+  i = 0
+  while i <= n
+    j = a[i]
+    p [j, j * 2 + 2] if a.include?(j * 2 + 2)
+    p [j, j * 3 + 3] if a.include?(j * 3 + 3)
+    a << j * 2 + 2
+    a << j * 3 + 3
+    a.sort!.uniq!
+    i += 1
+  end
+  a[0..n - 1]
+end
+
+n = 10000
+ary = A(n)
