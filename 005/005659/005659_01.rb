f2ea26fb4bfe0ee231d54a38c5dbdfe45620ae64
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-29 23:17:09 +0900

    If k appears so do 2k-2 and 3k-3.

---
+++ 005/005659/005659_01.rb
@@ -0,0 +1,22 @@
+# If n appears so do 2n-2 and 3n-3.
+def A(n)
+  a = [4]
+  i = 0
+  while i <= n
+    a << a[i] * 2 - 2
+    a << a[i] * 3 - 3
+    a.sort!.uniq!
+    i += 1
+  end
+  a[0..n - 1]
+end
+
+n = 10000
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
