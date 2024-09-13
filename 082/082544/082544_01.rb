75edbbf8c114894c9dd6e95b8fc02a8ded9cd852
manman4 <7023566+manman4@users.noreply.github.com>
2024-09-13 18:27:12 +0900

    Number of ordered quintuples (a,b,c,d,e) with gcd(a,b,c,d,e)=1 (1<= {a,b,c,d,e} <= n).

---
+++ 082/082544/082544_01.rb
@@ -0,0 +1,16 @@
+# a(n) = n^k - Sum_{j=2..n} a(floor(n/j)).
+def A(n, k)
+  ary = [0]
+  (1..n).each{|i|
+    ary << i ** k - (2..i).inject(0){|s, j| s + ary[i / j]}
+  }
+  ary
+end
+
+n = 10000
+ary = A(n, 5)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
