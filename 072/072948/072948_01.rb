485c2fc6b685c08c0d4181c41a5df8dd0fc8869c
manman4 <7023566+manman4@users.noreply.github.com>
2025-01-14 22:56:23 +0900

    Number of permutations p of {1,2,3,...,2n} such that Sum_{k=1..2n} abs(k-p(k)) = 2n.

---
+++ 072/072948/072948_01.rb
@@ -0,0 +1,24 @@
+def A(n, k)
+  k = n
+  dp = Array.new(n+1){Array.new(n+1){Array.new(k+1,0)}}
+  dp[0][0][0] = 1
+  n.times{|i|
+    (i+1).times{|j|
+      (k+1).times{|l|
+        next if k < l+2*(i-j)
+        dp[i+1][j+1][l+2*(i-j)] += dp[i][j][l]
+        dp[i+1][j  ][l+2*(i-j)] += dp[i][j][l]
+        dp[i+1][j+1][l+2*(i-j)] += dp[i][j][l] * 2 * (i-j)
+        dp[i+1][j+2][l+2*(i-j)] += dp[i][j][l] * (i-j) * (i-j) if j+2 <= n
+      }
+    }
+  }
+  dp[n][n][k]
+end
+
+n = 300
+(0..n).each{|i|
+  print i
+  print ' '
+  puts A(2 * i, i)
+}
