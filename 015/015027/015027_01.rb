441a54fa1f86b56c164a5512489556cb7f7197bb
manman4 <7023566+manman4@users.noreply.github.com>
2025-05-31 20:30:23 +0900

    q-factorial numbers for q=-12.

---
+++ 015/015027/015027_01.rb
@@ -0,0 +1,14 @@
+# A(n,k) = Product_{j=1..n} (1 - (-k)^j)/(1 + k).
+def A(n, k)
+  return 1 if n == 0
+  (1..n).inject(1){|s, i| s * (1 - (-k)**i) / (1 + k)}
+end
+
+n = 40
+(0..n).each{|i| 
+  j = A(i, 12)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
