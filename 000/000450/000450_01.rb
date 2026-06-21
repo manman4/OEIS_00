4c10da8cc19a3e03bed1bdaa143b8b632073f871
manman4 <7023566+manman4@users.noreply.github.com>
2026-06-21 21:47:26 +0900

    Coefficients of ménage hit polynomials.

---
+++ 000/000450/000450_01.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=4..n} (-1)^k * (n-k)! * binomial(k,4) * binomial(2*n-k,k).
+def a(n)
+  (4..n).inject(0){|s, k| s + (-1)**k * f(n-k) * ncr(k, 4) * ncr(2*n-k, k)}
+end
+
+n = 1000
+(4..n).each{|i|
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
